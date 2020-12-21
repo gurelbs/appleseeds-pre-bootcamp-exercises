b5b8eb99c1330f8a5fa6cc06716e3aa401916a68
gurelbs <guri240@gmail.com>
2020-12-21 05:16:02 +0200

    first commit

---
+++ ex1/ex1.js
@@ -0,0 +1,7 @@
+const input = require('readline-sync');
+
+let name = input.question("\n\n Hi! please Enter your name: \n\n");
+console.log(`\nHello, ${name}!
+my name is Gurel Ben Shabat.
+nice to meet you!
+:)`)
