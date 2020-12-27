c781346a3d142c6c46b29cd753065ff5f85f747f
gurelbs <guri240@gmail.com>
2020-12-27 09:23:22 +0200

    ex10

---
+++ ex10/ex10.js
@@ -0,0 +1,8 @@
+var readline = require('readline-sync');
+
+let str = readline.question('\nplease write something:\t', {
+    limit: str => str.length > 0,
+    limitMessage: `Sorry, $<lastInput> you didn't type anything. try again...`
+  })
+console.log(`\nhere is you'r new output:\n`);
+console.log(str.split(' ').join('*'));
