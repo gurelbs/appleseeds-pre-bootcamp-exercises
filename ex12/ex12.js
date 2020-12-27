78820a629950e19b9973d1d3862e61b74363cf73
gurelbs <guri240@gmail.com>
2020-12-27 11:57:04 +0200

    ex12

---
+++ ex12/ex12.js
@@ -0,0 +1,13 @@
+let readline = require('readline-sync');
+
+let str = readline.question('\nplease write something:\t', {
+    limit: str => str.length > 0,
+    limitMessage: `Sorry, '$<lastInput>' it's not valid. You must type at least 1 characters. try again...`
+})
+
+let vowel = () => {
+    console.log(`\nhere is you'r output:\n`);
+    str = str.replace(/[aeiouy]/g, char => char.toUpperCase())
+    console.log(str);
+}
+vowel()
